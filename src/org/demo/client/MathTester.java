57788cb34abd565e9532a6fd15c08d96915e92ed
prashant <prashant170599@gmail.com>
2022-03-13 00:20:59 +0530

    prashant Initial commit

---
+++ src/org/demo/client/MathTester.java
@@ -0,0 +1,16 @@
+package org.demo.client;
+
+import org.demo.tools.Calculator;
+
+public class MathTester {
+
+	public static void main(String[] args)
+	{
+	  Calculator c1=new Calculator();
+	  System.out.println(c1.add(3, 7));
+	  System.out.println(c1.substract(13, 7));
+	  System.out.println(c1.multiply(3, 7));
+	 
+	}
+
+}
---
+++ src/org/demo/tools/Calculator.java
@@ -0,0 +1,21 @@
+package org.demo.tools;
+
+public class Calculator {
+	int result;
+
+	public int add(int number1, int number2){
+		result = number1+number2;
+		return result;
+	}
+	
+	public int substract(int number1, int number2){
+		result = number1-number2;
+		return result;
+	}
+	public int multiply(int number1, int number2){
+		result = number1*number2;
+		return result;
+	}
+	
+	
+}
